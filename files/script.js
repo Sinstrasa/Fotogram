b42104831a57f56a1c770c1ad0ae36ca3ef68adb
Erik S. <eriksom764@gmail.com>
2026-06-05 10:06:51 +0200

    inserted function close

--- files/script.js
+++ files/script.js
@@ -1,4 +1,13 @@
+function initialise() {
+    
+}
+
 function openDialog() {
     let dialogRef = document.getElementById("dialog_window");
     dialogRef.showModal();
+}
+
+function closeDialog() {
+    let dialogRef = document.getElementById("dialog_window");
+    dialogRef.close();
 }
